01c2241bc70b60ea7dc3f6050612c957af7c1958
itacirgabral <itacirgabral@users.noreply.github.com>
2018-12-17 22:23:37 -0300

    reborn

---
+++ share/states.js
@@ -0,0 +1,142 @@
+exports.states = [
+  { 
+    "Instancia": {
+      "Dia": "D1",
+      "Perspectiva": "Ensolarado",
+      "Temperatura": "Quente",
+      "Umidade": "Alta",
+      "Vento": "Fraco"
+      },
+    "Classe": false
+   },
+  { 
+    "Instancia": {
+      "Dia": "D2",
+      "Perspectiva": "Ensolarado",
+      "Temperatura": "Quente",
+      "Umidade": "Alta",
+      "Vento": "Forte"
+      },
+    "Classe": false
+   },
+  { 
+    "Instancia": {
+      "Dia": "D3",
+      "Perspectiva": "Nublado",
+      "Temperatura": "Quente",
+      "Umidade": "Alta",
+      "Vento": "Fraco"
+      },
+    "Classe": true
+   },
+  { 
+    "Instancia": {
+      "Dia": "D4",
+      "Perspectiva": "Chuvoso",
+      "Temperatura": "Moderada",
+      "Umidade": "Alta",
+      "Vento": "Fraco"
+      },
+    "Classe": true
+   },
+  { 
+    "Instancia": {
+      "Dia": "D5",
+      "Perspectiva": "Chuvoso",
+      "Temperatura": "Fresca",
+      "Umidade": "Normal",
+      "Vento": "Fraco"
+      },
+    "Classe": true
+   },
+  { 
+    "Instancia": {
+      "Dia": "D6",
+      "Perspectiva": "Chuvoso",
+      "Temperatura": "Fresca",
+      "Umidade": "Normal",
+      "Vento": "Forte"
+      },
+    "Classe": false
+   },
+  { 
+    "Instancia": {
+      "Dia": "D7",
+      "Perspectiva": "Nublado",
+      "Temperatura": "Fresca",
+      "Umidade": "Normal",
+      "Vento": "Forte"
+      },
+    "Classe": true
+   },
+  { 
+    "Instancia": {
+      "Dia": "D8",
+      "Perspectiva": "Ensolarado",
+      "Temperatura": "Moderada",
+      "Umidade": "Alta",
+      "Vento": "Fraco"
+      },
+    "Classe": false
+   },
+  { 
+    "Instancia": {
+      "Dia": "D9",
+      "Perspectiva": "Ensolarado",
+      "Temperatura": "Fresca",
+      "Umidade": "Normal",
+      "Vento": "Fraco"
+      },
+    "Classe": true
+   },
+  { 
+    "Instancia": {
+      "Dia": "D10",
+      "Perspectiva": "Chuvoso",
+      "Temperatura": "Moderada",
+      "Umidade": "Normal",
+      "Vento": "Fraco"
+      },
+    "Classe": true
+   },
+  { 
+    "Instancia": {
+      "Dia": "D11",
+      "Perspectiva": "Ensolarado",
+      "Temperatura": "Moderada",
+      "Umidade": "Normal",
+      "Vento": "Forte"
+      },
+    "Classe": true
+   },
+  { 
+    "Instancia": {
+      "Dia": "D12",
+      "Perspectiva": "Nublado",
+      "Temperatura": "Moderada",
+      "Umidade": "Alta",
+      "Vento": "Forte"
+      },
+    "Classe": true
+   },
+  { 
+    "Instancia": {
+      "Dia": "D13",
+      "Perspectiva": "Nublado",
+      "Temperatura": "Quente",
+      "Umidade": "Normal",
+      "Vento": "Fraco"
+      },
+    "Classe": true
+   },
+  { 
+    "Instancia": {
+      "Dia": "D14",
+      "Perspectiva": "Chuvoso",
+      "Temperatura": "Moderada",
+      "Umidade": "Alta",
+      "Vento": "Forte"
+      },
+    "Classe": false
+   },
+]
