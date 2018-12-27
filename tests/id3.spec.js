a9ae93af5e3294c2bfb5df650e70f63121b6007f
itacirgabral <itacirgabral@users.noreply.github.com>
2018-12-26 22:22:40 -0300

    travis add

--- tests/id3.spec.js
+++ tests/id3.spec.js
@@ -7,8 +7,6 @@ const expected = {}
 test('id3 training terminal node', t => {
   dataset.terminal = [
     [[true], [['k1', 'v1']]],
-    [[true], [['k1', 'v1']]],
-    [[false], [['k1', 'v2']]],
     [[false], [['k1', 'v2']]]
   ]
   expected.terminal = {
@@ -23,18 +21,52 @@ test('id3 training terminal node', t => {
   t.deepEqual(value, expected.terminal)
 })
 
-test.skip('id3 training binary', t => {
-  // O.o it shoul fail!
+test('id3 training binary', t => {
   dataset.binary = [
-    [[true], [['k1', 'v1'], ['k2', 'v1']]],
-    [[true], [['k1', 'v1'], ['k2', 'v2']]],
-    [[true], [['k1', 'v2'], ['k2', 'v1']]],
-    [[false], [['k1', 'v2'], ['k2', 'v2']]]
+    [[true], [['k1', 'v1'], ['k2', 'v3']]],
+    [[false], [['k1', 'v1'], ['k2', 'v3']]],
+    [[false], [['k1', 'v2'], ['k2', 'v4']]],
+    [[true], [['k1', 'v2'], ['k2', 'v4']]]
   ]
   expected.binary = {
-    'k1': {
-      'v1': true,
-      'v2': false
+    'k2': {
+      'v3': {
+        'k1': {
+          'v1': false
+        }
+      },
+      'v4': {
+        'k1': {
+          'v2': false
+        }
+      }
+    }
+  }
+
+  const { gain, gridsheet, ...value } = train({ dataset: dataset.binary })
+
+  t.deepEqual(value, expected.binary)
+})
+
+test('id3 training repeted values', t => {
+  dataset.binary = [
+    [[true], [['k1', 'v1'], ['k2', 'v1']]],
+    [[false], [['k1', 'v1'], ['k2', 'v2']]],
+    [[false], [['k1', 'v2'], ['k2', 'v1']]],
+    [[true], [['k1', 'v2'], ['k2', 'v2']]]
+  ]
+  expected.binary = {
+    'k2': {
+      'v3': {
+        'k1': {
+          'v1': false
+        }
+      },
+      'v4': {
+        'k1': {
+          'v2': false
+        }
+      }
     }
   }
 
